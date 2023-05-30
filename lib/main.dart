cbae10f86712d8cfe958eb9f0904db4f9fd18efb
zu128 <90748869+zu128@users.noreply.github.com>
2023-05-30 17:58:37 +0900

    create menuCard widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:meokjago/l10n/l10n.dart';
-//import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:meokjago/screens/dummy_screen.dart';
 import 'package:meokjago/screens/home_screen.dart';
 import 'package:meokjago/screens/login_screen.dart';
+import 'package:meokjago/screens/search_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       supportedLocales: L10n.all,
       localizationsDelegates: const [
-        //AppLocalizations.delegate,
+        AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
@@ -52,7 +53,7 @@ class _appFrameState extends State<appFrame> {
   late List<BottomNavigationBarItem> items;
   List<Widget> page = [
     const dummyScreen(),
-    const dummyScreen(),
+    const searchScreen(),
     const homeScreen(),
     const dummyScreen(),
     const dummyScreen()
